5642be80eee2ff080117d6a9979498ef83b34ed0
hyperitsme <154733693+hyperitsme@users.noreply.github.com>
2025-10-29 13:02:33 +0000

    Create hmac.js

---
+++ src/utils/hmac.js
@@ -0,0 +1,6 @@
+import crypto from "crypto";
+import { CONFIG } from "../config.js";
+
+export function hmacBase64(payload) {
+  return crypto.createHmac("sha256", CONFIG.SERVER_SECRET).update(payload).digest("base64");
+}
