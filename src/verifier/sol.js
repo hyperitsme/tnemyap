edd1f941d059c2b74071b7ae72f62c532687168e
hyperitsme <154733693+hyperitsme@users.noreply.github.com>
2025-10-29 15:27:55 +0000

    Delete src/verifier/sol.js

--- src/verifier/sol.js
+++
@@ -1,4 +0,0 @@
-// TODO: Implement verifikasi SPL Token Program (Phantom).
-export async function verifySolSplTransfer(_args) {
-  return { ok: false, reason: "SPL verification not implemented yet in v1" };
-}
