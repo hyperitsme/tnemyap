c95c2c2de96d243ea5d206ba55986c475f65d195
hyperitsme <154733693+hyperitsme@users.noreply.github.com>
2025-10-29 13:04:04 +0000

    Create sol.js

---
+++ src/verifier/sol.js
@@ -0,0 +1,4 @@
+// TODO: Implement verifikasi SPL Token Program (Phantom).
+export async function verifySolSplTransfer(_args) {
+  return { ok: false, reason: "SPL verification not implemented yet in v1" };
+}
